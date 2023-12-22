884086655b95f8ced4c89206456adf46dfbed007
ymimemsm <mismysty@gmail.com>
2023-12-22 06:44:20 +0200

    completed finalProject

---
+++ js/01-gallery.js
@@ -0,0 +1,66 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+const container = document.querySelector(".gallery");
+
+function createMarkup(arr) {
+  return arr
+    .map(
+      ({ preview, original, description }) =>
+        `<li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+  </li>`
+    )
+    .join("");
+}
+
+container.insertAdjacentHTML("afterbegin", createMarkup(galleryItems));
+container.addEventListener("click", handlerImgClick);
+function handlerImgClick(event) {
+  event.preventDefault();
+
+  if (event.currentTarget === event.target) {
+    return;
+  }
+  const { source, alt: description } = event.target.dataset;
+  showImgModal({ original: source, preview: source, description });
+}
+
+function showImgModal({ preview, original, description }) {
+  const instance = basicLightbox.create(
+    `
+    <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+       
+      />`,
+    {
+      handler: null,
+      onShow(instance) {
+        this.handler = onEscape.bind(instance);
+        document.addEventListener("keydown", this.handler);
+      },
+      onClose() {
+        document.removeEventListener("keydown", this.handler);
+      },
+    }
+  );
+
+  instance.show();
+}
+
+function onEscape({ code }) {
+  if (code === "Escape") {
+    this.close();
+  }
+}
+
+console.log(galleryItems);
---
+++ js/02-lightbox.js
@@ -0,0 +1,34 @@
+import { galleryItems } from "./gallery-items.js";
+
+const container = document.querySelector(".gallery");
+
+function createMarkup(arr) {
+  return arr
+    .map(
+      ({ original, preview, description }) =>
+        `<li class="gallery__item">
+           <a class="gallery__link" href="${original}">
+              <img class="gallery__image" src="${preview}" alt="${description}" />
+           </a>
+         </li>`
+    )
+    .join("");
+}
+
+container.insertAdjacentHTML("afterbegin", createMarkup(galleryItems));
+
+const captlightbox = () => {
+  const lightbox = new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionDelay: 250,
+  });
+};
+
+captlightbox();
+
+// document.addEventListener("DOMContentLoaded", () => {
+//   const lightbox = new SimpleLightbox(".gallery a", {
+//     captionsData: "alt",
+//     captionDelay: 250,
+//   });
+// });
